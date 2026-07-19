a6efb2f241dc889efc450583745ed983fd4802bd
Матвеев Сергей Андреевич <matveevsa@biocad.ru>
2026-07-20 00:39:31 +0300

    Создать промокод в Mapper

--- Homeworks/03 Подключение базы данных через Entity Framework/src/PromoCodeFactory.WebHost/Mapping/PromoCodesMapper.cs
+++ Homeworks/03 Подключение базы данных через Entity Framework/src/PromoCodeFactory.WebHost/Mapping/PromoCodesMapper.cs
@@ -34,4 +34,35 @@ public static class PromoCodesMapper
             customerPromoCode.CreatedAt,
             customerPromoCode.AppliedAt);
     }
+
+    public static PromoCode ToPromoCode(
+        PromoCodeCreateRequest request,
+        Employee partnerManager,
+        Preference preference,
+        IEnumerable<Customer> customers)
+    {
+        var promoCodeId = Guid.NewGuid();
+        var createdAt = DateTimeOffset.UtcNow;
+
+        return new PromoCode
+        {
+            Id = promoCodeId,
+            Code = request.Code,
+            ServiceInfo = request.ServiceInfo,
+            PartnerName = request.PartnerName,
+            BeginDate = request.BeginDate,
+            EndDate = request.EndDate,
+            PartnerManager = partnerManager,
+            Preference = preference,
+            CustomerPromoCodes = customers
+                .Select(customer => new CustomerPromoCode
+                {
+                    Id = Guid.NewGuid(),
+                    CustomerId = customer.Id,
+                    PromoCodeId = promoCodeId,
+                    CreatedAt = createdAt
+                })
+                .ToList()
+        };
+    }
 }
