975562397d6e21382de69c0448027f90773284bd
Матвеев Сергей Андреевич <matveevsa@biocad.ru>
2026-07-20 00:18:23 +0300

    Добавляем в PromoCodesMapper.cs метод ToCustomerPromoCodeResponse

--- Homeworks/03 Подключение базы данных через Entity Framework/src/PromoCodeFactory.WebHost/Mapping/PromoCodesMapper.cs
+++ Homeworks/03 Подключение базы данных через Entity Framework/src/PromoCodeFactory.WebHost/Mapping/PromoCodesMapper.cs
@@ -17,4 +17,21 @@ public static class PromoCodesMapper
             promoCode.PartnerManager.Id,
             promoCode.Preference.Id);
     }
+
+    public static CustomerPromoCodeResponse ToCustomerPromoCodeResponse(
+        CustomerPromoCode customerPromoCode,
+        PromoCode promoCode)
+    {
+        return new CustomerPromoCodeResponse(
+            promoCode.Id,
+            promoCode.Code,
+            promoCode.ServiceInfo,
+            promoCode.PartnerName,
+            promoCode.BeginDate,
+            promoCode.EndDate,
+            promoCode.PartnerManager.Id,
+            promoCode.Preference.Id,
+            customerPromoCode.CreatedAt,
+            customerPromoCode.AppliedAt);
+    }
 }
