70ab31335421fbdbb3c4156b563b9198f126a97b
Матвеев Сергей Андреевич <matveevsa@biocad.ru>
2026-07-28 18:15:34 +0300

    Add Constructor CreateTests

--- Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/PromoCodes/CreateTests.cs
+++ Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/PromoCodes/CreateTests.cs
@@ -1,7 +1,34 @@
+using Moq;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.WebHost.Controllers;
+
 namespace PromoCodeFactory.UnitTests.WebHost.Controllers.PromoCodes;
 
 public class CreateTests
 {
+    private readonly Mock<IRepository<PromoCode>> _promoCodesRepositoryMock;
+    private readonly Mock<IRepository<Customer>> _customersRepositoryMock;
+    private readonly Mock<IRepository<CustomerPromoCode>> _customerPromoCodesRepositoryMock;
+    private readonly Mock<IRepository<Partner>> _partnersRepositoryMock;
+    private readonly Mock<IRepository<Preference>> _preferencesRepositoryMock;
+    private readonly PromoCodesController _sut;
+
+    public CreateTests()
+    {
+        _promoCodesRepositoryMock = new Mock<IRepository<PromoCode>>();
+        _customersRepositoryMock = new Mock<IRepository<Customer>>();
+        _customerPromoCodesRepositoryMock = new Mock<IRepository<CustomerPromoCode>>();
+        _partnersRepositoryMock = new Mock<IRepository<Partner>>();
+        _preferencesRepositoryMock = new Mock<IRepository<Preference>>();
+        _sut = new PromoCodesController(
+            _promoCodesRepositoryMock.Object,
+            _customersRepositoryMock.Object,
+            _customerPromoCodesRepositoryMock.Object,
+            _partnersRepositoryMock.Object,
+            _preferencesRepositoryMock.Object);
+    }
+
     [Fact]
     public async Task Create_WhenPartnerNotFound_ReturnsNotFound()
     {
