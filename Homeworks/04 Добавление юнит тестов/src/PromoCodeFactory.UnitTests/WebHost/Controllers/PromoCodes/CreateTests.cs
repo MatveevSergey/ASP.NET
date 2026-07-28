9ba40aacb8f9b737de85376047a54c0afdd019e3
Матвеев Сергей Андреевич <matveevsa@biocad.ru>
2026-07-28 18:21:20 +0300

    Implementation Create_WhenNoActiveLimit_ReturnsUnprocessableEntity()

--- Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/PromoCodes/CreateTests.cs
+++ Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/PromoCodes/CreateTests.cs
@@ -1,4 +1,5 @@
 using AwesomeAssertions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PromoCodeFactory.Core.Abstractions.Repositories;
@@ -7,6 +8,7 @@ using PromoCodeFactory.Core.Domain.PromoCodeManagement;
 using PromoCodeFactory.WebHost.Controllers;
 using PromoCodeFactory.WebHost.Models.PromoCodes;
 using Soenneker.Utils.AutoBogus;
+using System.Linq.Expressions;
 
 namespace PromoCodeFactory.UnitTests.WebHost.Controllers.PromoCodes;
 
@@ -88,6 +90,35 @@ public class CreateTests
     [Fact]
     public async Task Create_WhenNoActiveLimit_ReturnsUnprocessableEntity()
     {
+        // Arrange
+        var partnerId = Guid.NewGuid();
+        var preferenceId = Guid.NewGuid();
+        var partner = CreatePartner(partnerId, isActive: true);
+        var preference = CreatePreference(preferenceId, string.Empty);
+        var request = CreatePromoCodeCreateRequest(partnerId, preferenceId);
+
+        _partnersRepositoryMock
+            .Setup(r => r.GetById(partnerId, true, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(partner);
+
+        _preferencesRepositoryMock
+            .Setup(r => r.GetById(preferenceId, false, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(preference);
+
+        _customersRepositoryMock
+            .Setup(r => r.GetWhere(It.IsAny<Expression<Func<Customer, bool>>>(), false, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new List<Customer>());
+
+        // Act
+        var result = await _sut.Create(request, CancellationToken.None);
+
+        // Assert
+        result.Result.Should().BeOfType<ObjectResult>();
+        var objectResult = (ObjectResult)result.Result!;
+        objectResult.StatusCode.Should().Be(StatusCodes.Status422UnprocessableEntity);
+        objectResult.Value.Should().BeOfType<ProblemDetails>();
+        var problemDetails = (ProblemDetails)objectResult.Value!;
+        problemDetails.Title.Should().Be("No active limit");
     }
 
     [Fact]
@@ -116,6 +147,15 @@ public class CreateTests
             .RuleFor(p => p.IsActive, _ => isActive)
             .RuleFor(p => p.Manager, employee)
             .RuleFor(p => p.PartnerLimits, _ => new List<PartnerPromoCodeLimit>())
+            .Generate();
+    }
+
+    private static Preference CreatePreference(Guid preferenceId, string name)
+    {
+        return new AutoFaker<Preference>()
+            .RuleFor(p => p.Id, _ => preferenceId)
+            .RuleFor(p => p.Name, _ => name)
+            .RuleFor(p => p.Customers, _ => new List<Customer>())
             .Generate();
     }
 
