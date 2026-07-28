23f9809dc81e411fc1311bab197846e14e22e75d
Матвеев Сергей Андреевич <matveevsa@biocad.ru>
2026-07-28 18:18:09 +0300

    Implementation Create_WhenPartnerNotFound_ReturnsNotFound()

--- Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/PromoCodes/CreateTests.cs
+++ Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/PromoCodes/CreateTests.cs
@@ -1,7 +1,11 @@
+using AwesomeAssertions;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PromoCodeFactory.Core.Abstractions.Repositories;
 using PromoCodeFactory.Core.Domain.PromoCodeManagement;
 using PromoCodeFactory.WebHost.Controllers;
+using PromoCodeFactory.WebHost.Models.PromoCodes;
+using Soenneker.Utils.AutoBogus;
 
 namespace PromoCodeFactory.UnitTests.WebHost.Controllers.PromoCodes;
 
@@ -32,6 +36,24 @@ public class CreateTests
     [Fact]
     public async Task Create_WhenPartnerNotFound_ReturnsNotFound()
     {
+        // Arrange
+        var partnerId = Guid.NewGuid();
+        var preferenceId = Guid.NewGuid();
+        var request = CreatePromoCodeCreateRequest(partnerId, preferenceId);
+
+        _partnersRepositoryMock
+            .Setup(r => r.GetById(partnerId, true, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Partner?)null);
+
+        // Act
+        var result = await _sut.Create(request, CancellationToken.None);
+
+        // Assert
+        result.Result.Should().BeOfType<NotFoundObjectResult>();
+        var notFoundResult = (NotFoundObjectResult)result.Result!;
+        notFoundResult.Value.Should().BeOfType<ProblemDetails>();
+        var problemDetails = (ProblemDetails)notFoundResult.Value!;
+        problemDetails.Title.Should().Be("Partner not found");
     }
 
     [Fact]
@@ -52,5 +74,19 @@ public class CreateTests
     [Fact]
     public async Task Create_WhenValidRequest_ReturnsCreatedAndIncrementsIssuedCount()
     {
+    }
+
+    private static PromoCodeCreateRequest CreatePromoCodeCreateRequest(
+        Guid partnerId,
+        Guid preferenceId)
+    {
+        return new AutoFaker<PromoCodeCreateRequest>()
+            .RuleFor(p => p.Code, _ => Guid.NewGuid().ToString())
+            .RuleFor(p => p.ServiceInfo, _ => string.Empty)
+            .RuleFor(p => p.PartnerId, _ => partnerId)
+            .RuleFor(p => p.BeginDate, _ => DateTime.UtcNow)
+            .RuleFor(p => p.EndDate, _ => DateTime.UtcNow.AddDays(1))
+            .RuleFor(p => p.PreferenceId, _ => preferenceId)
+            .Generate();
     }
 }
