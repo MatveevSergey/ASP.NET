4148b74811d3210ae6b10e3238e069453ca17cca
Матвеев Сергей Андреевич <matveevsa@biocad.ru>
2026-07-28 18:03:16 +0300

    Add Constructor LimitTests

--- Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetLimitTests.cs
+++ Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetLimitTests.cs
@@ -1,7 +1,23 @@
+using Moq;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.WebHost.Controllers;
+
 namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners;
 
 public class SetLimitTests
 {
+    private readonly Mock<IRepository<Partner>> _partnersRepositoryMock;
+    private readonly Mock<IRepository<PartnerPromoCodeLimit>> _partnerLimitsRepositoryMock;
+    private readonly PartnersController _sut;
+
+    public SetLimitTests()
+    {
+        _partnersRepositoryMock = new Mock<IRepository<Partner>>();
+        _partnerLimitsRepositoryMock = new Mock<IRepository<PartnerPromoCodeLimit>>();
+        _sut = new PartnersController(_partnersRepositoryMock.Object, _partnerLimitsRepositoryMock.Object);
+    }
+
     [Fact]
     public async Task CreateLimit_WhenPartnerNotFound_ReturnsNotFound()
     {
