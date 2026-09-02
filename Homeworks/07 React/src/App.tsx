efe7f071881f06be872622df8997e5fafbbfcca5
Матвеев Сергей Андреевич <matveevsa@biocad.ru>
2026-09-02 13:28:34 +0300

    add hw07

---
+++ Homeworks/07 React/src/App.tsx
@@ -0,0 +1,10 @@
+import { FactFetcher } from "./components/FactFetcher.tsx";
+
+export default function App() {
+  return (
+    <main>
+      <h1>Cat facts</h1>
+      <FactFetcher />
+    </main>
+  );
+}
